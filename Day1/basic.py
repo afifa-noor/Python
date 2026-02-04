c48cd48657c6a65a751300fef445376339ac3817
Afifa Noor <i.afifanoor@gmail.com>
2026-02-04 19:18:54 +0530

    Added new python program

---
+++ Day1/basic.py
@@ -0,0 +1,19 @@
+print(5)
+
+print(5+3)
+
+a=5
+print(a)
+
+name="Afifa Noor"
+print(name)
+print("My name is" , name)
+
+a=10
+a=20
+print(a)
+
+lenght=10
+width=20
+area=lenght*width
+print("Area is", area)
