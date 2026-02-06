d428a12c06a81b72a1860e36f512f390d084db92
Afifa Noor <i.afifanoor@gmail.com>
2026-02-06 21:16:10 +0530

    Added new program

---
+++ Day2/variables.py
@@ -0,0 +1,25 @@
+# Variables are used to store data
+# A variable is created by assigning a value to it
+# The variable name is on the left side of the assignment operator (=)
+# The value is on the right side of the assignment operator (=)
+# A variable can be of any data type (string, integer, float, etc.)
+# Variables are case-sensitive (age and Age are different variables)
+# variable name can contain letters, numbers, and underscores, but cannot start with a number
+# variable name cannot be special characters (except for underscores)
+# Example of creating variables
+name = "Afifa"  # string variable    
+age = 21      # integer variable
+height = 1.75  # float variable
+print(name)  
+print(age)   
+print(height)
+# You can also assign the same value to multiple variables
+x = y = z = 10
+print(x)  
+print(y)  
+print(z)  
+# You can also assign different values to multiple variables in one line
+a, b, c = 1, 2, 3
+print(a)  
+print(b) 
+print(c)  
