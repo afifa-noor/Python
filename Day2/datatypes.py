4d042cafadb853cead7b768842699ede361459e8
Afifa Noor <i.afifanoor@gmail.com>
2026-02-06 21:33:39 +0530

    Added new program

---
+++ Day2/datatypes.py
@@ -0,0 +1,38 @@
+# Datatypes are the types of data that we can use in Python. They are used to store and manipulate data. The most common datatypes in Python are:
+# 1. String: A string is a sequence of characters enclosed in quotes. It can
+#    be created using single quotes (' '), double quotes (" "), or triple quotes (''' ''' or """ """).
+# 2. Integer: An integer is a whole number without a decimal point. It can
+#    be positive, negative, or zero.
+# 3. Float: A float is a number with a decimal point. It can be positive,
+#    negative, or zero.
+# 4. Boolean: A boolean is a datatype that can only have two values: True or False.
+# 5. List: A list is a collection of items that are ordered and changeable. It is created using square brackets [] and can contain items of different datatypes.
+# 6. Tuple: A tuple is a collection of items that are ordered and unchangeable. It is created using parentheses () and can contain items of different datatypes.
+# 7. Dictionary: A dictionary is a collection of key-value pairs that are unordered and changeable. It is created using curly braces {} and each key is separated from its value by a colon (:).
+# 8. Set: A set is a collection of unique items that are unordered and changeable. It is created using curly braces {} or the set() function.
+# Example of different datatypes
+name = "Afifa"  # string variable
+age = 21      # integer variable    
+height = 1.75  # float variable
+is_student = True  # boolean variable
+fruits = ["apple", "banana", "cherry"]  # list variable
+coordinates = (10.0, 20.0)  # tuple variable
+person = {"name": "Afifa", "age": 21}  # dictionary variable
+unique_numbers = {1, 2, 3}  # set variable
+print(name)
+print(age)
+print(height)
+print(is_student)
+print(fruits)
+print(coordinates)
+print(person)
+print(unique_numbers)
+# You can also check the datatype of a variable using the type() function
+print(type(name))  
+print(type(age))  
+print(type(height))  
+print(type(is_student))  
+print(type(fruits))  
+print(type(coordinates))  
+print(type(person))  
+print(type(unique_numbers))  # <class 'set'>
