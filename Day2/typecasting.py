b3ee9eab3c5c6699472dea0e81ef9f958ea34391
Afifa Noor <i.afifanoor@gmail.com>
2026-02-08 23:02:33 +0530

    Added new program

---
+++ Day2/typecasting.py
@@ -0,0 +1,35 @@
+# Typecasting is the process of converting a variable from one type to another. 
+# 1. int(): Converts a value to an integer
+# 2. float(): Converts a value to a float
+# 3. str(): Converts a value to a string
+# 4. bool(): Converts a value to a boolean
+
+#Converting a string to an integer
+a="10"
+b=int(a)
+print(b)  
+print(type(b))
+
+#Converting a string to a float
+c="3.14"
+d=float(c)  
+print(d)
+print(type(d))
+
+#Converting an integer to a string
+e=42
+f=str(e)
+print(f)
+print(type(f))
+
+#converting a float to an string
+g=15.99
+h=(str(g))
+print(h)
+print(type(h))
+
+#converting a float to integer
+i=13.22
+j=(int(i))
+print(j)
+print(type(j))
