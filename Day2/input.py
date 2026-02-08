895e1cc32e71b89775915a02a27fcc79046b5c25
Afifa Noor <i.afifanoor@gmail.com>
2026-02-08 23:42:01 +0530

    Added new program

---
+++ Day2/input.py
@@ -0,0 +1,15 @@
+#Taking input from the user
+number = input("Enter a number:")  
+print("You entered:", number)
+
+#Adding two numbers
+a = input("Enter the first number:")
+a=int(a)
+b = input("Enter the second number:")
+b=int(b)
+print(a+b)
+
+#Another way to add two numbers
+x = int(input("Enter the first number:"))
+y = int(input("Enter the second number:"))
+print(x+y)
