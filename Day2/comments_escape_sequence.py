cc101e3f8610d8c9f0e878aa795c4576bb8d3a47
Afifa Noor <i.afifanoor@gmail.com>
2026-02-15 23:16:14 +0530

    Added new program

---
+++ Day2/comments_escape_sequence.py
@@ -0,0 +1,26 @@
+#Comments #This is a comment, it is not executed by the program
+#Comments are used to explain code and make it more readable
+#This is a single-line comment
+#You can also use comments to temporarily disable code
+#This is a multi-line comment
+#You can use triple quotes to create multi-line comments
+"""
+This is a multi-line comment
+It can span multiple lines
+It is useful for explaining complex code or providing documentation
+"""
+#Escape sequence
+#An escape sequence is a combination of characters that represents a special character
+#It is used to include special characters in a string that would otherwise be difficult to include
+#Common escape sequences
+#\n: New line
+#\t: Tab
+#\\: Backslash
+#Example of using escape sequences
+print("Hello\nWorld")  # This will print Hello and World on separate lines
+print("Hello\tWorld")  # This will print Hello and World with a tab space in between
+print("This is a backslash: \\")  # This will print a single backslash
+#You can also use escape sequences to include quotes in a string
+print("She said, \"Hello!\"")  # This will print She said, "Hello!"
+print('It\'s a nice day!')  # This will print It's a nice day!
+print("This is a multi-line string using triple quotes.\nIt can span multiple lines.\nIt is useful for including special characters like new lines and tabs.\n\tThis line is indented with a tab.")     
